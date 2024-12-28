6683371638581f1a8f82b67fe09ea3e2910eb577
이정호 <hoya21120@naver.com>
2024-12-28 12:01:31 +0900

    for문 야구게임 예제

---
+++ practice/for-statement.py
@@ -0,0 +1,33 @@
+# 선생님 너네들 태도 점수를 점수에 넣겠다. (수행평가)
+# 1학기에 점수 배점이 중간 30 / 기말 30 / 배드민턴 30 / 태도점수 10
+# 어느 날 체육선생님이 아주 기분이 안좋은 날에
+# 그 전시간 수학선생님이 설명을 너무 열심히 하는 바람에
+# 단체로 체육시간에 늦음.
+# 체육선생님의 일괄적으로 1점씩 깎겠다.
+"""
+attitudes = [9,9,9,8,9,9,8,9,9,9]
+
+for attitude in attitudes:
+    print(attitude)
+    attitude = attitude + 1
+
+print("attitude에 값을 더한 것 - attribute: call by value")
+print(attitudes)
+
+for i in range(len(attitudes)):
+    attitudes[i] = attitudes[i] + 1
+
+print("attitudes에 더한 값을 넣어준 것 - attribute[i]: call by reference")
+print(attitudes)
+"""
+
+
+# 10명이 10문제를 하는 쪽지시험
+# 모두 객관식으로 이루어져 있음
+# 모든 배점을 10점씩
+# 학생은 0번 학생 ~ 9번 학생
+# 문제도 0번 문제 ~ 9번 문제
+
+
+
+
